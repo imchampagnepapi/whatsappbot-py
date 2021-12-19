6cf87dc5ef02227213d581d554adc5c08c864068
dbh4ck <16207658+dbh4ck@users.noreply.github.com>
2021-12-19 03:42:37 -0800

    make it better

--- app.py
+++ app.py
@@ -79,7 +79,12 @@ def sms_reply():
         resp = MessagingResponse()
         song_url_list = jio_query(str(msg[5:]))
         if len(song_url_list) > 0:
-            msg = resp.message()
+            msg = resp.message(f'''
+            Title: {song_url_list[0].title}
+            Album: {song_url_list[0].album}
+            Artist: {song_url_list[0].artist}
+            ''')
+            msg.media(song_url_list[0].thumb_url)
             msg.media(song_url_list[0].url)
         else:
             msg = resp.message(f"No song found for {msg[5:]}")
